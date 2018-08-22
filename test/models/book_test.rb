aa6f2f73ce79c10d9e790de5ed8e4cbbe33fbe16
Junichi Ito <jit@sonicgarden.jp>
2018-08-23 04:43:50 +0900

    Refine test

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -8,19 +8,32 @@ class BookTest < ActiveSupport::TestCase
     ralph = Author.create! name: 'ラルフ'
     eric = Author.create! name: 'エリック'
 
-    book.authors << eric
-    book.authors << ralph
-    book.authors << richard
-    book.authors << john
+    book.authors << [eric, ralph, richard, john]
 
+    # Author#id順
     assert_equal(
       [john, richard, ralph, eric],
       book.authors.order(:id)
     )
+    puts book.authors.order(:id).to_sql
+    # SELECT "authors".*
+    # FROM "authors"
+    # INNER JOIN "book_authors"
+    # ON "authors"."id" = "book_authors"."author_id"
+    # WHERE "book_authors"."book_id" = 1
+    # ORDER BY "authors"."id" ASC
 
+    # Authorの追加順
     assert_equal(
       [eric, ralph, richard, john],
       book.authors.order('book_authors.id')
     )
+    puts book.authors.order('book_authors.id').to_sql
+    # SELECT "authors".*
+    # FROM "authors"
+    # INNER JOIN "book_authors"
+    # ON "authors"."id" = "book_authors"."author_id"
+    # WHERE "book_authors"."book_id" = 1
+    # ORDER BY book_authors.id
   end
 end
