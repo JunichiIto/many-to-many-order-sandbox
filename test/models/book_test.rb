c7277cc7f9edbfa87fe4068848bcceeb70ddffa0
Junichi Ito <jit@sonicgarden.jp>
2018-08-23 04:37:24 +0900

    Add test for author order

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class BookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'author order' do
+    book = Book.create! title: 'デザインパターン'
+    john = Author.create! name: 'ジョン'
+    richard = Author.create! name: 'リチャード'
+    ralph = Author.create! name: 'ラルフ'
+    eric = Author.create! name: 'エリック'
+
+    book.authors << eric
+    book.authors << ralph
+    book.authors << richard
+    book.authors << john
+
+    assert_equal(
+      [john, richard, ralph, eric],
+      book.authors.order(:id)
+    )
+
+    assert_equal(
+      [eric, ralph, richard, john],
+      book.authors.order('book_authors.id')
+    )
+  end
 end
